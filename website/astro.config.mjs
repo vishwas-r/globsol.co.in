de6dec417f221cbe786f6c275132409808230377
Vishwas R <vishwas.r@fenopix.com>
2026-07-24 13:38:47 +0530

    Configure GitHub Pages subpath base URL for static assets

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://globsol.co.in',
+  site: 'https://vishwas-r.github.io',
+  base: '/globsol.co.in/',
 });
-
